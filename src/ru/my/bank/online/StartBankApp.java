7069a68f53afc5ef6ddb9333822b5a8128c63bd7
chuvilinsa <mtghunt@yandex.ru>
2022-01-10 19:43:04 +0300

    Array removed. Wrong logic added

--- src/ru/my/bank/online/StartBankApp.java
+++ src/ru/my/bank/online/StartBankApp.java
@@ -3,10 +3,7 @@ package ru.my.bank.online;
 public class StartBankApp {
 
     public static void main(String[] args) {
-        int[] array = {1,2,3};
-        for(int i : array) {
-            System.out.println(i);
-        }
+
         System.out.println("Starting my Online bank!!!");
     }
 }
